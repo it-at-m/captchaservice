d51b80b05f3b925fe4d86d39bc1c7b4e7fb536df
Tobias Holler <tobias.holler@muenchen.de>
2025-06-04 12:09:09 +0200

    created initial migration

---
+++ captchaservice-backend/src/main/resources/db/migration/schema/V001__Init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE captcha_request
+(
+    id                  UUID                        NOT NULL,
+    source_address_hash VARCHAR(64)                 NOT NULL,
+    valid_until         TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT pk_captcharequest PRIMARY KEY (id)
+);
+
+CREATE TABLE invalidated_payload
+(
+    id           UUID                        NOT NULL,
+    payload_hash VARCHAR(64)                 NOT NULL,
+    valid_until  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT pk_invalidatedpayload PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_captcha_request_source_address_hash ON captcha_request (source_address_hash);
+
+CREATE INDEX idx_invalidated_payload_payload_hash ON invalidated_payload (payload_hash);
