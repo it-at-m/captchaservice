6cd9e9b5c2d93fd04c652c2bbdddf709870dec87
Fabinatix97 <fabian@fabi-online.de>
2025-08-08 09:47:31 +0200

    Use Lombok annotations and add Javadoc for 'took' field

--- captchaservice-backend/src/main/java/de/muenchen/captchaservice/data/ExtendedPayload.java
+++ captchaservice-backend/src/main/java/de/muenchen/captchaservice/data/ExtendedPayload.java
@@ -1,17 +1,17 @@
 package de.muenchen.captchaservice.data;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.altcha.altcha.Altcha;
 
+@Getter
+@Setter
 public class ExtendedPayload extends Altcha.Payload {
 
+    /**
+     * The time taken (in milliseconds) to solve the captcha challenge.
+     * This field tracks how long it took for the user/client to complete the captcha.
+     */
     private Long took;
-
-    public Long getTook() {
-        return took;
-    }
-
-    public void setTook(Long took) {
-        this.took = took;
-    }
 
 }
