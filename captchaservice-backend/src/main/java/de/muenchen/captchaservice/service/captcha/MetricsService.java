e3b6a5536fa74f3e6bdf0724476b278748ec9bb9
Fabinatix97 <fabian@fabi-online.de>
2025-08-14 11:18:13 +0200

    Add tag to track request counts by same source address

--- captchaservice-backend/src/main/java/de/muenchen/captchaservice/service/captcha/MetricsService.java
+++ captchaservice-backend/src/main/java/de/muenchen/captchaservice/service/captcha/MetricsService.java
@@ -1,6 +1,7 @@
 package de.muenchen.captchaservice.service.captcha;
 
 import de.muenchen.captchaservice.data.SourceAddress;
+import de.muenchen.captchaservice.repository.CaptchaRequestRepository;
 import de.muenchen.captchaservice.repository.InvalidatedPayloadRepository;
 import de.muenchen.captchaservice.service.difficulty.DifficultyService;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -20,19 +21,24 @@ public class MetricsService {
     private final MeterRegistry meterRegistry;
     private final DifficultyService difficultyService;
     private final InvalidatedPayloadRepository invalidatedPayloadRepository;
+    private final CaptchaRequestRepository captchaRequestRepository;
 
     @SuppressFBWarnings(value = { "EI_EXPOSE_REP2" }, justification = "Dependency Injection")
     public MetricsService(MeterRegistry meterRegistry, DifficultyService difficultyService,
-            InvalidatedPayloadRepository invalidatedPayloadRepository) {
+            InvalidatedPayloadRepository invalidatedPayloadRepository, CaptchaRequestRepository captchaRequestRepository) {
         this.meterRegistry = meterRegistry;
         this.difficultyService = difficultyService;
         this.invalidatedPayloadRepository = invalidatedPayloadRepository;
+        this.captchaRequestRepository = captchaRequestRepository;
     }
 
-    public void recordChallengeRequest(String siteKey, long difficulty) {
+    public void recordChallengeRequest(String siteKey, long difficulty, SourceAddress sourceAddress) {
+        long sameSourceAddressRequestCount = getSameSourceAddressRequestCount(sourceAddress);
+
         Counter.builder("captcha.challenge.requests")
                 .tag("site_key", siteKey)
                 .tag("difficulty", String.valueOf(difficulty))
+                .tag("same_source_address_request_count", String.valueOf(sameSourceAddressRequestCount))
                 .description("Counter for captcha challenge requests")
                 .register(meterRegistry)
                 .increment();
@@ -40,10 +46,12 @@ public class MetricsService {
 
     public void recordVerifySuccess(String siteKey, SourceAddress sourceAddress) {
         final long difficulty = difficultyService.getDifficultyForSourceAddress(siteKey, sourceAddress);
+        long sameSourceAddressRequestCount = getSameSourceAddressRequestCount(sourceAddress);
 
         Counter.builder("captcha.verify.success")
                 .tag("site_key", siteKey)
                 .tag("difficulty", String.valueOf(difficulty))
+                .tag("same_source_address_request_count", String.valueOf(sameSourceAddressRequestCount))
                 .description("Counter for captcha verify success requests")
                 .register(meterRegistry)
                 .increment();
@@ -56,14 +64,20 @@ public class MetricsService {
         }
 
         final long difficulty = difficultyService.getDifficultyForSourceAddress(siteKey, sourceAddress);
+        long sameSourceAddressRequestCount = getSameSourceAddressRequestCount(sourceAddress);
 
         DistributionSummary.builder("captcha.client.solve.time")
                 .tag("site_key", siteKey)
                 .tag("difficulty", String.valueOf(difficulty))
+                .tag("same_source_address_request_count", String.valueOf(sameSourceAddressRequestCount))
                 .description("Summary of the time taken by clients to solve captcha challenges")
                 .baseUnit("milliseconds")
                 .register(meterRegistry)
                 .record(solveTime);
+    }
+
+    private long getSameSourceAddressRequestCount(SourceAddress sourceAddress) {
+        return captchaRequestRepository.countBySourceAddressHashIgnoreCaseAndExpiresAtGreaterThanEqual(sourceAddress.getHash(), Instant.now());
     }
 
     public void initializeInvalidatedPayloadsGauge() {
