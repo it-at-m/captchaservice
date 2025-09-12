fb44dba6e4f61c840b16e405499ed65b38b5f619
Fabinatix97 <fabian@fabi-online.de>
2025-09-12 21:07:02 +0200

    Temporarily disable checkExpires

--- captchaservice-backend/src/main/java/de/muenchen/captchaservice/service/captcha/CaptchaService.java
+++ captchaservice-backend/src/main/java/de/muenchen/captchaservice/service/captcha/CaptchaService.java
@@ -57,7 +57,7 @@ public class CaptchaService {
             return false;
         }
         try {
-            final boolean isValid = Altcha.verifySolution(payload, captchaProperties.hmacKey(), true);
+            final boolean isValid = Altcha.verifySolution(payload, captchaProperties.hmacKey(), false);
             log.info("Altcha.verifySolution() returned {} for payloadHash={}", isValid, getPayloadHash(payload));
             if (isValid) {
                 log.info("Sucessfully verified. Recording success for metrics...");
