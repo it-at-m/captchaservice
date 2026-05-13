47356e0b49e7ec7c003982d9b146cee4b55721f5
thomasafink <thomas.fink@muenchen.de>
2026-05-13 16:02:37 +0200

    clean(CAPTCHA): apply spotless formatting

--- captchaservice-backend/src/main/java/de/muenchen/captchaservice/configuration/filter/nfcconverter/NfcRequestFilter.java
+++ captchaservice-backend/src/main/java/de/muenchen/captchaservice/configuration/filter/nfcconverter/NfcRequestFilter.java
@@ -1,11 +1,11 @@
 package de.muenchen.captchaservice.configuration.filter.nfcconverter;
 
+import de.muenchen.captchaservice.util.LogSanitizer;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import de.muenchen.captchaservice.util.LogSanitizer;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
