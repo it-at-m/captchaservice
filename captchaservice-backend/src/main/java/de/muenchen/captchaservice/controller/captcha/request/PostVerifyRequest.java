d7c34791234b48e18f6a6a50eaf969474f7ec166
thomasafink <thomas.fink@muenchen.de>
2026-05-13 15:47:54 +0200

    fix(CAPTCHA): exclude payload from PostVerifyRequest toString
    Avoid default Object.toString on ExtendedPayload for CodeQL java/call-to-object-tostring.

--- captchaservice-backend/src/main/java/de/muenchen/captchaservice/controller/captcha/request/PostVerifyRequest.java
+++ captchaservice-backend/src/main/java/de/muenchen/captchaservice/controller/captcha/request/PostVerifyRequest.java
@@ -7,8 +7,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@ToString(exclude = "payload")
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostVerifyRequest {
